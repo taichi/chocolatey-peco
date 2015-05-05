b15588b893d7299d20c89829ba6a46b4bb401c39
Natsuki Mori <morinatsu@gmail.com>
2015-05-06 00:48:56 +0900

    deprecated: Write-ChocolateySuccess, Write-ChocolateyFailure

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-﻿$packageName = "peco"
+$packageName = "peco"
 $leaf = Split-Path $env:chocolateyPackageFolder -Leaf
 $version = $leaf.Remove(0, $packageName.Length+1)
 $url = "https://github.com/peco/peco/releases/download/v$version/peco_windows_386.zip"
