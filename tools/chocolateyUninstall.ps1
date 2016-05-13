1df1b1887147426ae32d4d08d531aa0cd732200e
sato taichi <sato.taichi@isid.co.jp>
2016-05-13 16:52:28 +0900

    remove unnecessary file.

--- tools/chocolateyUninstall.ps1
+++
@@ -1,9 +0,0 @@
-$packageName = "peco"
-$zipFileName = "pecoInstall.zip"
-
-$packagelibPath=$env:chocolateyPackageFolder
-$zipContentFile=(join-path $packagelibPath $zipFileName) + ".txt"
-$zipContents=get-content $zipContentFile
-foreach ($fileInZip in $zipContents) {
-  remove-item "$fileInZip" -ErrorAction SilentlyContinue
-}
