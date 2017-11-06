f76bdf43fa97b2ca467c6350c5920285e450d8c1
taichi <ryushi@gmail.com>
2017-11-07 01:16:19 +0900

    upgrade peco to 0.5.1

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName = "peco"
-$version = "0.4.7"
+$version = "0.5.1"
 $url = "https://github.com/peco/peco/releases/download/v$version/peco_windows_386.zip"
 $url64 = "https://github.com/peco/peco/releases/download/v$version/peco_windows_amd64.zip"
 
